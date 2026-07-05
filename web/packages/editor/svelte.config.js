3aa10fe7dd5e579f19b3fbb46500513e327c6bf3
Philip Oesterlin <philip.oesterlin@gmail.com>
2026-07-05 14:40:12 +0200

    Configure SvelteKit CSRF trusted origins
    Specify allowed origins for Cross-Site Request Forgery (CSRF) protection based on the `DOMAIN` environment variable. This enhances security by ensuring only requests from known origins are accepted.

--- web/packages/editor/svelte.config.js
+++ web/packages/editor/svelte.config.js
@@ -1,13 +1,20 @@
 import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
+const trustedOrigins = [process.env.DOMAIN];
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 
-  kit: {
-    adapter: adapter(),
-  },
+	kit: {
+		adapter: adapter(),
+		csrf: {
+			trustedOrigins
+		}
+	}
 };
 
 export default config;
